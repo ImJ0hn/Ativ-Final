9bc9d1b9355aa6dc554146685fa4b17176431e2b
ImJ0hn <jhonathan66945@gmail.com>
2023-06-03 23:40:53 -0300

    Não está cadastrando.

---
+++ app/Http/Controllers/ControllerContatos.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\ModelContatos;
+use Illuminate\Http\Request;
+
+class ControllerContatos extends Controller
+{
+
+    public function CadastrarContato(Request $request){
+
+        $validarDados = $request->validate(['nome' => 'required', 'telefone'  => 'required', 'email'  => 'required']);
+
+        
+
+        $novoContato = ModelContatos::create([
+            'nome' => $request->input('nome'),
+            'telefone' => $request->input('telefone'),
+            'email' => $request->input('email'),
+        ]);
+        
+        
+        
+    }
+
+
+    // function CadastrarContato(Request $request){
+
+    //     $modelCont = new ModelContatos();
+    //     $modelCont->nome = $request->input('Nome');
+    //     $modelCont->email = $request->input('E-mail');
+    //     $modelCont->telefone = $request->input('Telefone');
+    
+    //     $modelCont->save();
+
+    //     return view('index');
+    // }
+
+}
