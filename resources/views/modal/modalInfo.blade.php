39811b81e3850ff69d415d1a5d6004bfc14444a2
ImJ0hn <jhonathan66945@gmail.com>
2023-06-16 04:26:42 -0300

    Modal com as informações aparecendo..

---
+++ resources/views/modal/modalInfo.blade.php
@@ -0,0 +1,41 @@
+
+
+
+<div class="modal fade" id="ModalInfo{{$contato->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Informações do contato</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+
+
+      <div class="modal-body">
+          <div class="mb-3">
+            
+            <label for="InputNome" class="col-form-label">Nome:</label>
+            <input type="text" readonly class="form-control" id="InputNome" name="nome" value="{{$contato->nome}}" required>
+          </div>
+
+          <div class="mb-3">
+            <label for="InputTelefone" class="col-form-label">Telefone:</label>
+            <input type="text" readonly class="form-control" id="InputTelefone" name="telefone" value="{{$contato->telefone}}" required>
+          </div>
+
+          <div class="mb-3">
+            <label for="InputEmail" class="col-form-label">E-mail:</label>
+            <input type="email" readonly class="form-control" id="InputEmail" name="email" value="{{$contato->email}}" required>
+          </div>
+
+     
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+    </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
